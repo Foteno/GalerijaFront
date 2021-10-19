0cb3200d715f7cd82c76f314dd8938358d33e68b
Foteno <patrikass32@gmail.com>
2021-10-19 16:10:06 +0300

    t

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeleteComponent } from './delete/delete.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailsComponent } from './image-details/image-details.component';
+import { UploadComponent } from './upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/gallery', pathMatch: 'full' },
+  { path: 'upload', component: UploadComponent },
+  { path: 'delete', component: DeleteComponent },
+  { path: 'image/:uuid', component:  ImageDetailsComponent},
+  { path: 'gallery', component: GalleryComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UploadComponent } from './upload/upload.component';
+import { DeleteComponent } from './delete/delete.component';
+import { ImageDetailsComponent } from './image-details/image-details.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    UploadComponent,
+    DeleteComponent,
+    ImageDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatPaginatorModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
